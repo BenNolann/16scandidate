f1da138ace9561152bd61198334f4dcb1048aad7
Ben Nolan <alexandranolan@SamsMacbookpro.local>
2018-10-31 16:57:53 +0000

    Add script that, given organism name, returns a fasta file containing the 16S gene for each available hit on greengenes

--- scripts/sort16greengenes.sh
+++ scripts/sort16greengenes.sh
@@ -1,9 +1,26 @@
+#!/bin/bash 
 # Given an organism genus and species name, take out the 16S data from greengenes.
+# Requires $1 to be a a file only containing the id and accessions of the organism of interest
+ 
 
-INPUT= $1
-OUTPUT= $2
+
+OUTPUT_DIR=$1
+
+if [ -d "$OUTPUT_DIR" ] 
+then
+    echo "Directory exists." 
+else
+    echo "Creating directory"
+    mkdir $OUTPUT_DIR
+fi
 
 
- while read id acc; do echo $acc;
- cat ./Desktop/FYP/datam/greengenes/gg_13_5.fasta | grep -A 1 ">$id" >> $2; 
-  done < $1
+
+# Input organism name after grep
+grep "Methanofollis liminatans" /Users/alexandranolan/Desktop/16scandidate/greengenes/greenidacc > $OUTPUT_DIR/green_acid
+
+
+while read id acc
+  do echo $acc
+ cat /Users/alexandranolan/Desktop/FYP/datam/greengenes/gg_13_5.fasta | grep -A 1 ">$id$" >> $OUTPUT_DIR/green_acid.fasta 
+  done < $OUTPUT_DIR/green_acid
