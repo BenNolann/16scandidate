231dd7a028dc9dd81dfddb3b8e846a3245baa66e
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2018-11-02 15:53:10 +0000

    Add improvements to sra section of the script

--- scripts/ncbribo.sh
+++ scripts/ncbribo.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-INPUT_FILE=$1
-OUTPUT_DIR=$2
+OUTPUT_DIR=$1
 
 #Download reads for a specific organism from NCBI based on results of ggrrndbsra.R
 #INPUT_FILE containing accessions obtained from ggrrndbsra.R result for chosen organism.
-while read acc
-  do
-    fastq-dump --split-files $acc
-  echo $acc
-  done < $INPUT_FILE 
+
+if [ -z "$OUTPUT_DIR" ]
+  then
+echo "Directory exists"
+  else
+echo "Creating directory"
+  mkdir $OUTPUT_DIR
+fi
 
 
+grep "Pediococcus acidilactici" /Users/alexandranolan/Desktop/16scandidate/srafind/srapure >$OUTPUT_DIR/srafind 
+
+while read sra gs 
+  do echo $sra
+    cat /Users/alexandranolan/Desktop/16scandidate/srafind/srapure | grep -A 1 ">$sra$"
+    fastq-dump --split-files $sra  > $OUTPUT_DIR/sra.fastq 
+done < $OUTPUT_DIR/srafind 
+
+
+exit
 
 #Use pyani to find the best reference genome for each genome
 
-for file in genome_files
- do 
-   run_ANI.sh -e EXPERIMENTNAME -f FORWARD -r REVERSE  -n 10 -o $OUTPUT_DIR
-  echo $file
-done
+for sra in $OUTPUT_DIR/sra.fastq
+  do 
+    run_ANI.sh -e ORGANISMNAME -f FORWARD -r REVERSE  -n 10 -o $OUTPUT_DIR
+   echo $sra
+ done
 
 #Use riboseed to assemble the reads 
 
-for file in riboseed_files 
- do
- ribo run -r  -F  -R  --cores 16 --threads 1 -v 1 --serialize -o 
-  echo $file
-done
+#for file in riboseed_files 
+ #do
+ #ribo run -r  -F  -R  --cores 16 --threads 1 -v 1 --serialize -o 
+  #echo $file
+#done
